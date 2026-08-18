13589473fb8597b4db00d62b736c2c9675477004
fluffybacon1 <bailyhohman0114@gmail.com>
2026-08-18 16:59:04 +0200

    ok

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -49,8 +49,8 @@ export default function TopBar({
           className="reaction"
           title="Photos that arrived without location data"
         >
-          <span className="glyph">📍</span>
-          <span>{unplacedCount} unplaced</span>
+          <span className="glyph"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q10 0 19.5.5T520-877v81q-10-2-20-3t-20-1q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186q122-112 181-203.5T720-552q0-2-.5-4t-.5-4h80q0 2 .5 4t.5 4q0 100-79.5 217.5T480-80Zm0-450Zm195-108 84-84 84 84 56-56-84-84 84-84-56-56-84 84-84-84-56 56 84 84-84 84 56 56ZM536.5-503.5Q560-527 560-560t-23.5-56.5Q513-640 480-640t-56.5 23.5Q400-593 400-560t23.5 56.5Q447-480 480-480t56.5-23.5Z"/></svg></span>
+          <span>{unplacedCount}</span>
         </button>
       )}
 
@@ -61,19 +61,19 @@ export default function TopBar({
       </Link>
 
       <Link href="/browse" className="btn btn-quiet btn-sm sm:hidden">
-        Gallery
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M360-400h400L622-580l-92 120-62-80-108 140Zm-40 160q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/></svg>
       </Link>
 
       <Link href="/upload" className="btn btn-primary btn-sm">
         Add photos
       </Link>
 
-      <button type="button" className="icon-btn icon-btn-sm" onClick={signOut} aria-label="Sign out">
+      {/* <button type="button" className="icon-btn icon-btn-sm" onClick={signOut} aria-label="Sign out">
         <svg width="15" height="15" viewBox="0 0 16 16" fill="none" aria-hidden="true">
           <path d="M6 2.6H3.4v10.8H6" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
           <path d="M9 5.2L11.8 8 9 10.8M11.6 8H6.2" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
         </svg>
-      </button>
+      </button> */}
     </header>
   );
 }
